299c3f6d2d12db3e58e66e3458c8d4cb7160b714
Jordan Mata <jordanmata15@gmail.com>
2018-11-03 21:49:45 -0700

    TwoDArray underlying container moved to stack and added operator= to copy contents across 2DArrays

--- src/TestTwoDArray.cpp
+++ src/TestTwoDArray.cpp
@@ -1,26 +1,95 @@
 #include <gtest/gtest.h>
+#include <limits.h>
+#include <iostream>
 #include "TwoDArray.hpp"
 
 class TestTwoDArray : public testing::Test{
   public:
     
-    TwoDArray<int> arrDef;
-    TwoDArray<int> arr;
     TwoDArray<int> * arrPtrDef;
-    TwoDArray<int> * arrPtr;
+    TwoDArray<int> * intArrPtr;
 
     virtual void SetUp(){
-      arrPtrDef = new TwoDArray<int>();
-      arrPtr = new TwoDArray<int>(25,35);
     }
 
-    virtual void TestDown(){
+    virtual void TearDown(){
+      delete arrPtrDef;
+      delete intArrPtr;
     }
 };
 
 TEST_F( TestTwoDArray, validSizeTest ){
-  //ASSERT_EQ(10,arr1.getNumRows());
-  //ASSERT_EQ(10,arr1.getNumCols());
+  std::cout << "TEST 1" << std::endl;
+  // Default constructor
+  TwoDArray<int> arrDef;
+  arrPtrDef = new TwoDArray<int>();
+  ASSERT_EQ(10,arrDef.getNumRows()); // Stack test
+  ASSERT_EQ(10,arrDef.getNumCols());
+  ASSERT_EQ(10,arrPtrDef->getNumRows()); // Heap test
+  ASSERT_EQ(10,arrPtrDef->getNumCols());
+
+  // #ROWS = #COLS
+  TwoDArray<int> arr(15,15);
+  intArrPtr = new TwoDArray<int>(25,25);
+  ASSERT_EQ(15,arr.getNumRows()); // Stack test
+  ASSERT_EQ(15,arr.getNumCols());
+  ASSERT_EQ(25,intArrPtr->getNumRows()); // Heap test
+  ASSERT_EQ(25,intArrPtr->getNumCols());
+  delete intArrPtr;
+
+  // #ROWS > #COLS
+  TwoDArray<int> arr1(27,13);
+  intArrPtr = new TwoDArray<int>(17,6);
+  ASSERT_EQ(27,arr1.getNumRows()); // Stack test
+  ASSERT_EQ(13,arr1.getNumCols());
+  ASSERT_EQ(17,intArrPtr->getNumRows()); // Heap test
+  ASSERT_EQ(6,intArrPtr->getNumCols());
+  delete intArrPtr;
+
+  // #ROWS < #COLS
+  TwoDArray<int> arr2(42,49);
+  intArrPtr = new TwoDArray<int>(18,1);
+  ASSERT_EQ(42,arr2.getNumRows()); // Stack test
+  ASSERT_EQ(49,arr2.getNumCols());
+  ASSERT_EQ(18,intArrPtr->getNumRows()); // Heap test
+  ASSERT_EQ(1,intArrPtr->getNumCols());
+  delete intArrPtr;
+
+  // #ROWS > 0, #COLS = 0
+  TwoDArray<int> arr3(42,0);
+  intArrPtr = new TwoDArray<int>(18,0);
+  ASSERT_EQ(42,arr3.getNumRows()); // Stack test
+  ASSERT_EQ(0,arr3.getNumCols());
+  ASSERT_EQ(18,intArrPtr->getNumRows()); // Heap test
+  ASSERT_EQ(0,intArrPtr->getNumCols());
+  delete intArrPtr;
+
+  // #ROWS = 0, #COLS > 0
+  TwoDArray<int> arr4(0,49);
+  intArrPtr = new TwoDArray<int>(0,1);
+  ASSERT_EQ(0,arr4.getNumRows()); // Stack test
+  ASSERT_EQ(49,arr4.getNumCols());
+  ASSERT_EQ(0,intArrPtr->getNumRows()); // Heap test
+  ASSERT_EQ(1,intArrPtr->getNumCols());
+  delete intArrPtr;
+  
+  // #ROWS = 0, #COLS = 0
+  TwoDArray<int> arr5(0,0);
+  intArrPtr = new TwoDArray<int>(0,0);
+  ASSERT_EQ(0,arr5.getNumRows()); // Stack test
+  ASSERT_EQ(0,arr5.getNumCols());
+  ASSERT_EQ(0,intArrPtr->getNumRows()); // Heap test
+  ASSERT_EQ(0,intArrPtr->getNumCols());
+  delete intArrPtr;
+  
+  // #ROWS/#COLS VERY LARGE
+  TwoDArray<int> arr6(INT_MAX,INT_MAX);
+  intArrPtr = new TwoDArray<int>(INT_MAX,INT_MAX);
+  ASSERT_EQ(INT_MAX,arr6.getNumRows()); // Stack test
+  ASSERT_EQ(INT_MAX,arr6.getNumCols());
+  ASSERT_EQ(INT_MAX,intArrPtr->getNumRows()); // Heap test
+  ASSERT_EQ(INT_MAX,intArrPtr->getNumCols());
+  
 }
 
 TEST_F( TestTwoDArray, invalidSizeTest ){
--- src/TwoDArray.hpp
+++ src/TwoDArray.hpp
@@ -10,7 +10,6 @@
 
 /* Description: Used to emulate a 2D vector using contigous memory. Stores 
  *              objects of generic type T.
- *              Underlying container allocated on heap to support larger sizes.
  */
 template<typename T>
 class TwoDArray{
@@ -22,7 +21,7 @@ class TwoDArray{
     int const DEF_COL_SIZE = 10;
     int numRows;
     int numCols;
-    std::vector<T> * vecPtr; // Underlying container
+    std::vector<T> vec; // Underlying container
 
   public:
     /* Default constructor, creates the container of default size
@@ -36,11 +35,15 @@ class TwoDArray{
      * arg2 - int n -- Indicates number of columns
      */
     TwoDArray( const int & m, const int & n ): numRows(m), 
-                numCols(n), vecPtr(new std::vector<T>(m*n)){}
+                numCols(n), vec(m*n){}
 
     /* Destructor that specifies the size of the 2D Array
      */
-    ~TwoDArray(){ delete vecPtr; }
+    ~TwoDArray(){ }
+    
+    /* Overload =operator to copy the contents
+     */
+    TwoDArray& operator=(const TwoDArray& x){this->vec=x.vec; return *this;}
 
     /* Getter method for accessing number of Rows. */
     int getNumRows(){ return numRows; }
@@ -54,7 +57,7 @@ class TwoDArray{
      * arg1 - int i -- Indicates number of rows
      * arg2 - int j -- Indicates number of rows
      */
-    T at( int i, int j ){ return vecPtr->at( i*(numCols)+j ); }
+    T at( int i, int j ){ return vec.at( i*(numCols)+j ); }
 
     /* Used to write to the (i,j) index of the 2D array.
      * Overwrites current object at that index.
@@ -63,7 +66,7 @@ class TwoDArray{
      * arg2 - int j -- Indicates number of rows
      * arg3 - T key -- Object of generic type T to insert
      */
-    void insert( int i, int j, T item ){ vecPtr->at( i*(numCols)+j ) = item; }
+    void insert( int i, int j, T item ){ vec.at( i*(numCols)+j ) = item; }
 
 };
 #endif // TWODARRAY_HPP
