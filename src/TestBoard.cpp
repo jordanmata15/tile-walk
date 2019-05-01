8f5538b272f4f7e256ffe29c109eb9fb06b5493f
Jordan Mata <jordanmata15@gmail.com>
2019-04-30 20:12:55 -0700

    Added test cases for load board and valid move tests

--- src/TestBoard.cpp
+++ src/TestBoard.cpp
@@ -2,30 +2,49 @@
 #include "Board.hpp"
 
 class TestBoard : public testing::Test{
-  public:
-    
+  
+  public:  
+    Board tenByTen;
+    Board sixBySix;
 
     virtual void SetUp(){
+      tenByTen = Board(10, 10,  ' ', ' ', ' ');
+      if(tenByTen.loadBoard("./BoardSamples/10x10MiddleFree.brd") == -1){
+        std::cerr << "FAILED TO READ FILE: BoardSamples/10x10MiddleFree.brd!\n";
+      }/*
+      sixBySix = Board(6, 6,  ' ', ' ', ' ');
+      if(tenByTen.loadBoard("./BoardSamples/6x6MiddleFree.brd") == -1){
+        std::cerr << "FAILED TO READ FILE: BoardSamples/6x6Full.brd!\n";
+      }*/
     }
 
     virtual void TestDown(){
     }
 };
-
+/*
 TEST_F( TestBoard, validMoveTest ){
-  //ASSERT_EQ(10, tenByTen);
+  tenByTen.setCurrIdx(3,4); // Above empty space
+  ASSERT_EQ(true, tenByTen.isValidMove('S'));
+    
+  tenByTen.setCurrIdx(4,3); // Left of empty space
+  ASSERT_EQ(true, tenByTen.isValidMove('D'));
+    
+  tenByTen.setCurrIdx(5,4); // Below empty space
+  ASSERT_EQ(true, tenByTen.isValidMove('W'));
+    
+  tenByTen.setCurrIdx(3,4); // Right of empty space
+  ASSERT_EQ(true, tenByTen.isValidMove('A'));
 }
 
 TEST_F( TestBoard, invalidMoveTest ){
 }
-
+*/
 TEST_F( TestBoard, loadBoard ){
-  Board tenByTen(10, 10,  ' ', ' ', ' ');
-  if(tenByTen.loadBoard("./BoardSamples/10x10Numbered.brd") == 0){
-    std::cout << std::endl;
-    tenByTen.displayBoard();
-    std::cout << std::endl;
-  }
+  // Valid Boards
+  ASSERT_EQ(0, tenByTen.loadBoard("./BoardSamples/10x10MiddleFree.brd"));
+  
+  // Invalid Boards
+  ASSERT_EQ(-1, tenByTen.loadBoard("./oardSamples/10x10MiddleFree.brd"));
 }
 
 int main(int argc, char* argv[]){
