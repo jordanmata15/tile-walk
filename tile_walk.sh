8d3c362153cdb3bdb3b4decee65c91ef49fb9057
Jordan Mata <jordanmata15@yahoo.com>
2019-12-07 13:30:31 -0800

    Updated path variables for running script and checking for build directory

--- tile_walk.sh
+++ tile_walk.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
-STATUS='./status'
+PROJECT=~/Projects/tile-walk
+STATUS=$PROJECT/status
+BUILD=$PROJECT/build
+
+#Create the build directory if it does not exist
+if [ ! -d $BUILD ]; then
+  echo 'Build directory does not exist! Making directory.'
+  mkdir $BUILD
+fi
 
 #Create the status directory if it does not exist
 if [ ! -d $STATUS ]; then
-  echo 'Directory does not exist! Making directory.'
+  echo 'Status directory does not exist! Making directory.'
   mkdir $STATUS
 fi
 
